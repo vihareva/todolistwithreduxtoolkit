5523bc71c162b9f29fcacf642a9991edb0bbab98
vihareva <viharevakatia@gmail.com>
2022-02-04 01:47:58 +0300

    Updates

---
+++ precache-manifest.be1047ff796e58ffa47c8863e3f01b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f871800ad433f570b7813cb2c5d48c1",
+    "url": "/todolistwithreduxtoolkit/index.html"
+  },
+  {
+    "revision": "f1cf6e37d87e8ef8014c",
+    "url": "/todolistwithreduxtoolkit/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "8e7e6b24a736dc09cc10",
+    "url": "/todolistwithreduxtoolkit/static/js/2.9d62ac06.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolistwithreduxtoolkit/static/js/2.9d62ac06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cf6e37d87e8ef8014c",
+    "url": "/todolistwithreduxtoolkit/static/js/main.16151e4e.chunk.js"
+  },
+  {
+    "revision": "e8c0ef8d1e504ca9e53f",
+    "url": "/todolistwithreduxtoolkit/static/js/runtime-main.14246691.js"
+  }
+]);
